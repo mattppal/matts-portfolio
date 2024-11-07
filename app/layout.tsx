9079a728ca183efcbaaf01ddb437283f61a84a91
Matt <59450879+mattppal@users.noreply.github.com>
2024-11-07 09:24:11 -0800

    projects v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="canonical" href="https://your-domain.com" />
+        <link rel="canonical" href="https://mattpalmer.io" />
         <JsonLd />
       </head>
       <body className={inter.className}>{children}</body>
