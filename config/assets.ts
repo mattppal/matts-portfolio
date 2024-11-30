8aa7076e711e88617d6bfb9e5efb61db3290c182
Matt Palmer <mattppalmer@gmail.com>
2024-11-30 15:16:22 -0800

    work with me

--- config/assets.ts
+++ config/assets.ts
@@ -1,79 +1,72 @@
 export const assets = {
-  bg: {
-    'bg-0': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-0.jpg',
-    'bg-1': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-1.jpg',
-    'bg-10': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-10.jpg',
-    'bg-11': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-11.jpg',
-    'bg-12': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-12.jpg',
-    'bg-13': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-13.jpg',
-    'bg-14': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-14.jpg',
-    'bg-15': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-15.jpg',
-    'bg-16': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-16.jpg',
-    'bg-17': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-17.jpg',
-    'bg-18': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-18.jpg',
-    'bg-19': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-19.jpg',
-    'bg-2': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-2.jpg',
-    'bg-3': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-3.jpg',
-    'bg-4': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-4.jpg',
-    'bg-5': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-5.jpg',
-    'bg-6': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-6.jpg',
-    'bg-7': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-7.jpg',
-    'bg-8': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-8.jpg',
-    'bg-9': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-9.jpg',
+  "bg": {
+    "bg-0": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-0.jpg",
+    "bg-1": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-1.jpg",
+    "bg-10": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-10.jpg",
+    "bg-11": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-11.jpg",
+    "bg-12": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-12.jpg",
+    "bg-13": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-13.jpg",
+    "bg-14": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-14.jpg",
+    "bg-15": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-15.jpg",
+    "bg-16": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-16.jpg",
+    "bg-17": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-17.jpg",
+    "bg-18": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-18.jpg",
+    "bg-19": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-19.jpg",
+    "bg-2": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-2.jpg",
+    "bg-3": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-3.jpg",
+    "bg-4": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-4.jpg",
+    "bg-5": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-5.jpg",
+    "bg-6": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-6.jpg",
+    "bg-7": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-7.jpg",
+    "bg-8": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-8.jpg",
+    "bg-9": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/bg/bg-9.jpg"
   },
-  headshot: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/headshot.jpg',
-  'hero-carousel': {
-    eatin: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/eatin.png',
-    'hiking-2':
-      'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/hiking-2.png',
-    hiking: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/hiking.png',
-    'speaking-2':
-      'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/speaking-2.png',
-    'speaking-3':
-      'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/speaking-3.png',
-    speaking: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/speaking.png',
-    'workin-2':
-      'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/workin-2.png',
-    workin: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/workin.png',
+  "headshot": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/headshot.jpg",
+  "hero-carousel": {
+    "eatin": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/eatin.png",
+    "hiking-2": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/hiking-2.png",
+    "hiking": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/hiking.png",
+    "speaking-2": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/speaking-2.png",
+    "speaking-3": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/speaking-3.png",
+    "speaking": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/speaking.png",
+    "workin-2": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/workin-2.png",
+    "workin": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/hero-carousel/workin.png"
   },
-  lifts: {
-    bench: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/lifts/bench.mp4',
-    dead: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/lifts/dead.mp4',
-    squat: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/lifts/squat.mp4',
+  "lifts": {
+    "bench": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/lifts/bench.mp4",
+    "dead": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/lifts/dead.mp4",
+    "squat": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/lifts/squat.mp4"
   },
-  'logo-carousel': {
-    Linkedin: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/Linkedin.svg',
-    a16z: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/a16z.svg',
-    bytewax: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/bytewax.svg',
-    'coalesce-logo':
-      'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/coalesce-logo.svg',
-    databricks:
-      'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/databricks.svg',
-    hex: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/hex.svg',
-    oreilly: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/oreilly.svg',
+  "logo-carousel": {
+    "Linkedin": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/Linkedin.svg",
+    "a16z": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/a16z.svg",
+    "bytewax": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/bytewax.svg",
+    "coalesce-logo": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/coalesce-logo.svg",
+    "databricks": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/databricks.svg",
+    "hex": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/hex.svg",
+    "oreilly": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/oreilly.svg",
+    "replit": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/replit.svg",
+    "tobiko": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/tobiko.svg",
+    "xai": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/xai.svg",
+    "yc": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/yc.svg"
+  },
+  "projects": {
     "og (1)": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/og%20(1).jpg",
-    replit: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/replit.svg',
-    tobiko: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/tobiko.svg',
-    xai: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/xai.svg',
-    yc: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/logo-carousel/yc.svg',
+    "uetl": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/uetl.png",
+    "vid2gif": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/vid2gif.gif",
+    "wtw": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/wtw.gif",
+    "x-receipts": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/x-receipts.gif",
+    "xai": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/xai.jpeg",
+    "yc": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/yc.jpeg",
+    "youtube": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/youtube.jpg"
   },
-  projects: {
-    uetl: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/uetl.png',
-    vid2gif: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/vid2gif.gif',
-    wtw: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/wtw.gif',
-    'x-receipts': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/x-receipts.gif',
-    xai: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/xai.jpeg',
-    yc: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/yc.jpeg',
-    youtube: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/projects/youtube.jpg',
-  },
-  replit: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/replit.svg',
-  robots: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/robots.txt',
-  sitemap: 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/sitemap.xml',
-  whitepapers: {
-    'orm-data-transformation':
-      'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/whitepapers/orm-data-transformation.pdf',
-    'orm-uetl': 'https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/whitepapers/orm-uetl.pdf',
-  },
+  "replit": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/replit.svg",
+  "robots": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/robots.txt",
+  "sitemap": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/sitemap.xml",
+  "whitepapers": {
+    "orm-data-transformation": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/whitepapers/orm-data-transformation.pdf",
+    "orm-uetl": "https://zdntzuxuw3xqvcia.public.blob.vercel-storage.com/whitepapers/orm-uetl.pdf"
+  }
 } as const;
 
 export type AssetUrl = string;
